5a778fd4b1a3c6c6aa53fc3f9b64f784db6ec2ae
ronsh138 <ronsherman2008@gmail.com>
2025-09-19 01:05:31 +0300

    updated and finished ping code

--- ping.py
+++ ping.py
@@ -1,6 +1,7 @@
 from scapy.all import IP, ICMP, sr1
 import socket
 import random
+import time
 
 retry = 2
 t_o = 0.5
@@ -10,15 +11,16 @@ def ping(ttl, host):
     id = 12345
     seq = random.randint(2, 60000)
     pk = (IP(dst=host, ttl=ttl)/ICMP(type = 8, id = id, seq = seq))
-
+    t = time.perf_counter()
     response = sr1(pk, timeout=t_o, verbose=False, retry=retry)
+    t2 = time.perf_counter()
 
     if response is not None:
-        print(f"{ttl}. current ip {response.src}, ttl = {ttl}, type = {response.type}")
+        print(f"{ttl}. HOST {response.src}, TTL {ttl}, time {(t2-t)*1000:.2f} [ms], Ping Response Type = {response.type}")
         if response.src == host:
             return ttl
     else:
-        print(f"{ttl}. no response")
+        print(f"{ttl}. .Warning: Host time out, TTL {ttl} Moving on .")
     return 0
 
 if "__main__" == __name__:
